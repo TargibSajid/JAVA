e4646b46ddcd7a679bbb77796f3b3403cfc3fa07
TargibSajid <167701006+TargibSajid@users.noreply.github.com>
2024-04-24 11:10:54 +0600

    Initial commit

---
+++ My works/Practices/DataTypes and varibales/Main.java
@@ -0,0 +1,22 @@
+class Main
+{
+	public static void main(String [] args)
+	{	
+		int b = 10;
+
+		long c = 5777;
+
+		float f = 7.5f;
+
+		double a = 10.5;
+
+
+		long k = (long)b;
+
+		System.out.println(a);
+
+		char arr[] = {'s','a','j','i','d'};
+
+		System.out.println(arr);
+	}
+}
